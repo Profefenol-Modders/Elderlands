801ff37f9fa89d5c8e09d05246c1d2848c4f6fee
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-06-14 22:26:53 -0400

    Round #4

--- src/main/java/io/github/catchyaintit/block/ModBlocks.java
+++ src/main/java/io/github/catchyaintit/block/ModBlocks.java
@@ -3,7 +3,7 @@ package net.catchyaintit.blocks;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.catchyaintit.elderland.ElderLand;
+import net.catchyaintit.ElderLand;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
