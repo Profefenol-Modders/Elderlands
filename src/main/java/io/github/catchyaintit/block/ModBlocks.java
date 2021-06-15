fe9d63596fbdba1d282642ca06a5f517043a0523
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-06-14 22:40:30 -0400

    Sometimes you're just dumb...

--- src/main/java/io/github/catchyaintit/block/ModBlocks.java
+++ src/main/java/io/github/catchyaintit/block/ModBlocks.java
@@ -1,9 +1,9 @@
-package net.catchyaintit.blocks;
+package io.github.catchyaintit.blocks;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.catchyaintit.ElderLand;
+import io.github.catchyaintit.ElderLand;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
