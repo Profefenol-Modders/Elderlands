4cf3bf821593084e5f973bb92ce7914e0ab64d53
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-06-14 22:02:51 -0400

    Hopefully add a new tab in the creative inventory
    This will allow for Elderlands to expand greatly! If only it works first time...

--- src/main/java/io/github/catchyaintit/ElderLand.java
+++ src/main/java/io/github/catchyaintit/ElderLand.java
@@ -3,6 +3,12 @@ package io.github.catchyaintit;
 import net.fabricmc.api.ModInitializer;
 
 public class ElderLand implements ModInitializer {
+	
+        public static final ItemGroup ELDER_GROUP = FabricItemGroupBuilder.create(
+        new Identifier(ElderLand.MOD_ID, "elder_group"))
+        .icon(() -> new ItemStack(ModItems.GILDED_ELDERAN_BLOCK))
+        .build();
+	
 	@Override
 	public void onInitialize() {
 
