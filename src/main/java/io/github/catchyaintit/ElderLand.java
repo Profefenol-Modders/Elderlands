257f21679b183f86d44a0ace0a0a5b0ec1d3b5a9
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-06-14 22:11:16 -0400

    Maybe it will not fail now?

--- src/main/java/io/github/catchyaintit/ElderLand.java
+++ src/main/java/io/github/catchyaintit/ElderLand.java
@@ -4,13 +4,25 @@ import net.fabricmc.api.ModInitializer;
 
 public class ElderLand implements ModInitializer {
 	
+        public static Logger LOGGER = LogManager.getLogger();
+
+        public static final String MOD_ID = "platinum";
+        public static final String MOD_NAME = "Platinum Mod";
+	
         public static final ItemGroup ELDER_GROUP = FabricItemGroupBuilder.create(
         new Identifier(ElderLand.MOD_ID, "elder_group"))
         .icon(() -> new ItemStack(ModItems.GILDED_ELDERAN_BLOCK))
         .build();
 	
-	@Override
-	public void onInitialize() {
+        @Override
+        public void onInitialize() {
+            log(Level.INFO, "Initializing");
 
-	}
+            ModItems.register();
+            ModBlocks.register();
+        }
+
+        public static void log(Level level, String message){
+            LOGGER.log(level, "["+MOD_NAME+"] " + message);
+        }
 }
