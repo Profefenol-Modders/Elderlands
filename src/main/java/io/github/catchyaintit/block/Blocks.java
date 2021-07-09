794a70a3b2804d19f869ba73e2ba33960580c9f9
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-07-08 22:51:02 -0400

    Update Blocks.java

--- src/main/java/io/github/catchyaintit/block/Blocks.java
+++ src/main/java/io/github/catchyaintit/block/Blocks.java
@@ -9,9 +9,11 @@ import net.minecraft.util.registry.Registry;
 
 public class Blocks {
     public static Block UNSTABLE_ELDERIUM = new Block(FabricBlockSettings.of(Material.METAL));
+    public static Block STABLE_ELDERIUM = new Block(FabricBlockSettings.of(Material.METAL));
 
     public static void register() {
         Registry.register(Registry.BLOCK, new Identifier(ElderLand.MODID, "unstable_elderium_item.json"), UNSTABLE_ELDERIUM);
+        Registry.register(Registry.BLOCK, new Identifier(ElderLand.MODID, "stable_elderium_item.json"), STABLE_ELDERIUM);
 
     }
 }
