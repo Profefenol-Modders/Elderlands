b4ec7e8c13a35a7ab47152845c840a5ae203d70e
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-01-24 22:27:57 -0500

    I don't even know anymore
    - Fixes something

--- src/main/java/io/github/profefenol/elderlands/gui/ModItemGroup.java
+++ src/main/java/io/github/profefenol/elderlands/gui/ModItemGroup.java
@@ -9,22 +9,22 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public abstract class ModItemGroup {
-        public static final ItemGroup ELDERLANDS_ITEMS = FabricItemGroupBuilder.build(
-                new Identifier("elderlands", "item_group"),
-                () -> new ItemStack(Items.UNSTABLE_SHARD));
+    public static final ItemGroup ELDERLANDS_ITEMS = FabricItemGroupBuilder.build(
+            new Identifier("elderlands", "item_group"),
+            () -> new ItemStack(Items.UNSTABLE_SHARD));
 
-        public static final ItemGroup ELDERLANDS_BLOCKS = FabricItemGroupBuilder.create(
-                        new Identifier("elderlands", "block_group"))
-                .icon(() -> new ItemStack(ModBlocks.UNSTABLE_ELDERIUM))
-                .build();
+    public static final ItemGroup ELDERLANDS_BLOCKS = FabricItemGroupBuilder.create(
+                    new Identifier("elderlands", "block_group"))
+            .icon(() -> new ItemStack(ModBlocks.UNSTABLE_ELDERIUM))
+            .build();
 
-        public static final ItemGroup ELDERLANDS_ARMOR = FabricItemGroupBuilder.create(
-                        new Identifier("elderlands", "armor_group"))
-                .icon(() -> new ItemStack(Armor.RegisterItems.ELDERIUM_MATERIAL_CHESTPLATE))
-                .build();
+    public static final ItemGroup ELDERLANDS_ARMOR = FabricItemGroupBuilder.create(
+                    new Identifier("elderlands", "armor_group"))
+            .icon(() -> new ItemStack(Armor.RegisterItems.ELDERIUM_MATERIAL_CHESTPLATE))
+            .build();
 
-        public static final ItemGroup ELDERLANDS_MISC = FabricItemGroupBuilder.create(
-                        new Identifier("elderlands", "misc_group"))
-                .icon(() -> new ItemStack(ModBlocks.PORTAL_FRAME))
-                .build();
+    public static final ItemGroup ELDERLANDS_MISC = FabricItemGroupBuilder.create(
+                    new Identifier("elderlands", "misc_group"))
+            .icon(() -> new ItemStack(ModBlocks.PORTAL_FRAME))
+            .build();
 }
